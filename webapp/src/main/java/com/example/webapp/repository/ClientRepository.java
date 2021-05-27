49a0f4a4213b0e0027cd097aaf2eca16f29038e3
git-sergBond <git-serg.bond@yandex.ru>
2021-05-28 00:29:27 +0300

    add Auth api

--- webapp/src/main/java/com/example/webapp/repository/ClientRepository.java
+++ webapp/src/main/java/com/example/webapp/repository/ClientRepository.java
@@ -3,5 +3,8 @@ package com.example.webapp.repository;
 import com.example.webapp.domain.Client;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends CrudRepository<Client, Long> {
+    Optional<Client> findByEmail(String email);
 }
--- webapp/src/main/java/com/example/webapp/service/AuthService.java
+++ webapp/src/main/java/com/example/webapp/service/AuthService.java
@@ -1,11 +1,18 @@
 package com.example.webapp.service;
 
 import api.dto.AuthPasswordDto;
+import com.example.webapp.repository.ClientRepository;
 import io.micrometer.core.instrument.util.StringUtils;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
+
+    private ClientRepository clientRepository;
+
+    public AuthService (ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
 
     public boolean login(AuthPasswordDto authPasswordDto) {
         if (authPasswordDto == null
@@ -13,6 +20,11 @@ public class AuthService {
                 || StringUtils.isBlank(authPasswordDto.getPassword())) {
             throw new IllegalArgumentException("wrong arguments");
         }
-        return authPasswordDto.getLogin().equals(authPasswordDto.getPassword());
+
+        boolean isAuthenticated = clientRepository.findByEmail(authPasswordDto.getLogin())
+                .map(client -> client.getPasswd().equals(authPasswordDto.getPassword()))
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        return isAuthenticated;
     }
 }
