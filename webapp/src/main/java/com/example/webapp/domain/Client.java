fa3110508c122a90f9290dfecb94d378067c1046
git-sergBond <git-serg.bond@yandex.ru>
2021-05-29 20:46:52 +0300

    add lib lombok

--- webapp/src/main/java/com/example/webapp/domain/Client.java
+++ webapp/src/main/java/com/example/webapp/domain/Client.java
@@ -1,10 +1,13 @@
 package com.example.webapp.domain;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.time.Instant;
 
 @Entity
+@Data
 public class Client {
 
     @Id
@@ -25,77 +28,4 @@ public class Client {
     String mobile_plone;
 
     String email;
-
-    public Client(String first_name,
-                  String middle_name,
-                  String last_name,
-                  String passwd,
-                  Instant birthday,
-                  String address,
-                  String mobile_plone,
-                  String email) {
-        this.first_name = first_name;
-        this.middle_name = middle_name;
-        this.last_name = last_name;
-        this.passwd = passwd;
-        this.birthday = birthday;
-        this.address = address;
-        this.mobile_plone = mobile_plone;
-        this.email = email;
-    }
-
-    public Client() {
-
-    }
-
-    @Override
-    public String toString() {
-        return "Client{" +
-                "id=" + id +
-                ", first_name='" + first_name + '\'' +
-                ", middle_name='" + middle_name + '\'' +
-                ", last_name='" + last_name + '\'' +
-                ", passwd='" + passwd + '\'' +
-                ", birthday=" + birthday +
-                ", address='" + address + '\'' +
-                ", mobile_plone='" + mobile_plone + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getFirst_name() {
-        return first_name;
-    }
-
-    public String getMiddle_name() {
-        return middle_name;
-    }
-
-    public String getLast_name() {
-        return last_name;
-    }
-
-    public String getPasswd() {
-        return passwd;
-    }
-
-    public Instant getBirthday() {
-        return birthday;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getMobile_plone() {
-        return mobile_plone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
 }
--- webapp/src/main/java/com/example/webapp/resource/AuthentificationResource.java
+++ webapp/src/main/java/com/example/webapp/resource/AuthentificationResource.java
@@ -4,19 +4,17 @@ import com.example.webapp.openapi.api.AuthentificationApi;
 import com.example.webapp.openapi.model.ClientDto;
 import com.example.webapp.openapi.model.EmailPasswordDto;
 import com.example.webapp.service.AuthService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class AuthentificationResource implements AuthentificationApi {
 
     private final AuthService authService;
-
-    public AuthentificationResource(AuthService authService) {
-        this.authService = authService;
-    }
 
     @Override
     public ResponseEntity<ClientDto> _signIn(@RequestBody EmailPasswordDto emailPasswordDto) {
